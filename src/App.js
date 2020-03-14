28db50854a616723da3ecadf3251f15a7b1bdb97
andriyfm <andrifmsyh@gmail.com>
2020-03-14 22:43:51 +0700

    Example: Dynamic Context

--- src/App.js
+++ src/App.js
@@ -1,54 +1,33 @@
 import React from "react";
 import "./App.css";
 
-const ThemeContext = React.createContext({ color: "light" });
-const UserContext = React.createContext("andri");
+import { ThemeContext, themes } from "./contexts/ThemeContext";
+import ThemedButton from "./components/ThemedButton";
+
+function Toolbar(props) {
+  return <ThemedButton onClick={props.changeTheme}>Change Theme</ThemedButton>;
+}
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { theme: themes.light };
+    this.toggleTheme = () => {
+      this.setState(state => ({
+        theme: state.theme === themes.dark ? themes.light : themes.dark
+      }));
+    };
+  }
+
   render() {
     return (
       <header class="App-header">
-        <ThemeContext.Provider value={{ color: "red" }}>
-          <UserContext.Provider value="firmansyah">
-            <Toolbar />
-          </UserContext.Provider>
+        <ThemeContext.Provider value={this.state.theme}>
+          <Toolbar changeTheme={this.toggleTheme} />
         </ThemeContext.Provider>
       </header>
     );
   }
-}
-
-class ThemeButton extends React.Component {
-  static contextType = UserContext;
-
-  render() {
-    // return <button>Button {this.context}</button>;
-    return (
-      <div>
-        <ThemeContext.Consumer>
-          {themeValue => (
-            <div>
-              <UserContext.Consumer>
-                {value => (
-                  <h1>
-                    {value} - {themeValue.color}
-                  </h1>
-                )}
-              </UserContext.Consumer>
-            </div>
-          )}
-        </ThemeContext.Consumer>
-      </div>
-    );
-  }
-}
-
-function Toolbar(props) {
-  return (
-    <div>
-      <ThemeButton />
-    </div>
-  );
 }
 
 export default App;

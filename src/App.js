9a6ebb0341164212e331b37415cff33749164e72
andriyfm <andrifmsyh@gmail.com>
2020-03-16 10:12:07 +0700

    Instal, import and initilalize the font awesome 5

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
 import { ThemeContext, themes } from "./contexts/ThemeContext";
-import ShortcodePages from "./pages/ShortcodesPage";
+
+// Import Layout and pages
+import Layout from "./components/Layout";
+
+// Import main style
 import "./styles/main.scss";
+
+// Import and intilize the font awesome 5
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fab, fas);
 
 class App extends React.Component {
   constructor(props) {
@@ -22,7 +33,9 @@ class App extends React.Component {
   render() {
     return (
       <ThemeContext.Provider value={this.state}>
-        <ShortcodePages />
+        <Layout>
+          <h1>Content here...</h1>
+        </Layout>
       </ThemeContext.Provider>
     );
   }

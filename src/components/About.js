dfc77104fc294cb6baeac33eeaac87e6a82cb819
andriyfm <andrifmsyh@gmail.com>
2020-03-16 23:03:56 +0700

    Styling about section

---
+++ src/components/About.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import SocialMedia from "./SocialMedia";
+
+const About = props => {
+  return (
+    <section className="about">
+      <Container>
+        <Row>
+          <Col lg={7}>
+            <h2 className="about__title">about us</h2>
+            <p className="about__text">
+              Mollit qui enim aliquip tempor. Commodo ut in excepteur cupidatat
+              dolore adipisicing dolore aliqua dolor laborum officia ex. Commodo
+              Lorem aliquip ad labore deserunt laborum consectetur.
+            </p>
+            <p className="about__text">
+              Mollit cupidatat nostrud laborum exercitation eu voluptate.
+              Laborum sunt et officia laboris. Et voluptate eiusmod est est
+              magna incididunt sint. Est do pariatur esse quis officia ex ea
+              nostrud sunt.
+            </p>
+            <SocialMedia />
+          </Col>
+          <Col lg={5}>
+            <img
+              className="about__thumbnail"
+              src={require("../assets/images/img1.jpg")}
+              alt="thumbnail"
+            />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Layout from "../components/Layout";
 import CTA from "../components/Cta";
-import Brands from "../components/Brands";
+import About from "../components/About";
 import CompanyInfo from "../components/CompanyInfo";
+import Brands from "../components/Brands";
 
 const Home = props => {
   return (
     <Layout>
+      <About />
       <CompanyInfo />
       <Brands />
       <CTA />
